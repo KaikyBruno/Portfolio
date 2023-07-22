08185cf74518761d1327052c60cb99fec1f2a83d
KaikyBruno <kaikybruno069@gmail.com>
2023-07-21 23:59:07 -0300

    create

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ function projetos () {
 
 const modedark = document.getElementById ('lua')
 const main = document.getElementById('main')
+const imagem = document.getElementById ('imagem')
 
 modedark.addEventListener ("click", () => {
     if (modedark.classList.contains ('fa-moon')) {
@@ -24,4 +25,45 @@ modedark.addEventListener ("click", () => {
         main.classList.remove ('light')
     }
 }) 
+if (window.scrollTo(0,0)){
+    btn.style.display = none 
+} else {
+    
+}
 
+const btn = document.getElementById ('button-arrow')
+
+btn.addEventListener ('click', function(){
+    window.scrollTo (0,0)
+})
+
+function ocultar() {
+    if (window.scrollY > 10) {
+        btn.style.display = 'flex'
+    } else {
+        btn.style.display = 'none'
+    }
+}
+
+document.addEventListener('scroll', ocultar)
+
+const hamburguer = document.getElementById ('icon-hamburguer')
+const backhamburguer = document.getElementById ('background-hamburguer')
+const lua = document.getElementById ('lua')
+
+hamburguer.addEventListener ('click', () => {
+    if (hamburguer.classList.contains ('fa-bars')) {
+        hamburguer.classList.remove ('fa-bars')
+        hamburguer.classList.add ('fa-outdent')
+        backhamburguer.style.display = 'flex'
+        lua.style.display = 'inline-block'
+
+        
+    } else {
+        hamburguer.classList.add ('fa-bars')
+        hamburguer.classList.remove ('fa-outdent')
+        backhamburguer.style.display = 'none'
+        lua.style.display = 'none'
+        
+    }
+})
